35a6dc129bde74bcc694ebccbfe960d58519b605
serhiy_mariuk <tyhyi@protonmail.com>
2024-10-02 22:14:53 +0300

    task 1

--- homework_3/task_1.py
+++ homework_3/task_1.py
@@ -1,4 +1,4 @@
 name = 'serhiy'
 day = '02.10.2024'
 
-print("Good day", name, "!",  day,  "is a perfect day to learn some python.")
+print("Good day", name,"!",  day,  "is a perfect day to learn some python.")
