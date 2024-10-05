0565c7c77519cf8b1136273a258e3004b7a5e4cc
serhiy_mariuk <tyhyi@protonmail.com>
2024-10-05 23:07:02 +0300

    task 4

--- homework_4/task_2.py
+++
@@ -1,5 +0,0 @@
-phone_number = input("Enter phone number:")
-if len(phone_number) == 10 and phone_number.isdigit():
-    print("You have entered the correct number format ")
-else:
-    print("Enter the phone number again")
