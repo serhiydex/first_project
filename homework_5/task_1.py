2090988de372d8eb240a4a723959698903f97fc3
serhiy_mariuk <tyhyi@protonmail.com>
2024-10-06 20:36:23 +0300

    Update task_1.py

--- homework_5/task_1.py
+++ homework_5/task_1.py
@@ -2,7 +2,6 @@ import random
 
 ch_1 = int(input("Enter a number between 1 and 10: "))
 ch_2 = random.randrange(1, 10)
-print(ch_2)
 while ch_1 != ch_2:
     ch_1 = int(input("Enter a number again: "))
 print("You guessed the number")
