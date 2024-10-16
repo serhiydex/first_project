68cf32136edaa5fdcbaebade5c40c8b88e27c31f
serhiydex <tyhyi@protonmail.com>
2024-10-16 09:44:44 +0300

    task_2

---
+++ homework_7/task_2.py
@@ -0,0 +1,16 @@
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+res = {}
+for key in stock:
+    res[key] = stock[key] * prices[key]
+print(res)
