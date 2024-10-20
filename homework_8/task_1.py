f6c8b432f184564d6899d4ce2cdafc593716f9cf
serhiy_mariuk <tyhyi@protonmail.com>
2024-10-20 21:09:19 +0300

    task_1

--- homework_8/task_1.py
+++ homework_8/task_1.py
@@ -1,5 +1,5 @@
 def favorite_movie(name):
-    print("My favorite movie is named", {name})
+    print(f"My favorite movie is named {name}")
 
 name = input("Input your favorite movie ")
 favorite_movie(name)
